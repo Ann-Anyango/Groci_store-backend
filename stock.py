a033ab6ad49e05d07e21df6a4f454bd2f66bbbe0
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-05-21 21:07:43 +0300

    created a class stock where mama mboga can update stock and remove some stock

--- stock.py
+++ stock.py
@@ -1,37 +1,33 @@
 
 class Stock:
- def __init__(self, product, quantity):
+ def __init__(self, product, quantity,price):
      self.product = product
      self.quantity = quantity
-
-
+     self.price = price
  def generate_purchase_order(self):
-       print(f"Added {self.quantity} kgs of {self.product}.")
-
-
+       print(f"Added {self.quantity} kgs of {self.product} worth {self.price*self.quantity}")
  def remove_stock(self, removed_quantity):
     if self.quantity >= removed_quantity:
         self.quantity -= removed_quantity
         print(f"Removed {removed_quantity} kgs of {self.product}. New stock quantity: {self.quantity} kgs.")
     else:
         print("Insufficient stock. Cannot remove requested quantity.")
-      
-
-
  def check_stock_level(self):
      if self.quantity>2000:
          print("stock is updated")
      else:
          print("stock needs to be updated")
-
-
  def update_quantity(self, new_quantity):
     self.quantity = new_quantity
     print(f"Updated quantity of {self.product} to {self.quantity} kgs.")
-
-
  def check_availability(self, required_quantity):
     if self.quantity >= required_quantity:
         print(f"{required_quantity} kgs of {self.product} are available in stock.")
     else:
         print(f"Only {self.quantity} kgs of {self.product} are available in stock. Insufficient quantity.")
+stock1 = Stock("Banana", 1340, 100)
+stock1.generate_purchase_order()
+stock1.remove_stock(350)
+stock1.check_stock_level()
+stock1.update_quantity(1560)
+stock1.check_availability(1400)
