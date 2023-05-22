a7b106844c0d46ad702b93f6534ddc4ff9351da7
Ann-Anyango <annmwangombe@gmail.com>
2023-05-22 10:37:26 +0300

    created a signing in and aloging in page that a customer puts all the information required

--- customer.py
+++ customer.py
@@ -1,10 +1,9 @@
 class Customer:
-    def __init__(self, first_name, second_name, email, password, confirm_password):
+    def __init__(self, first_name, second_name, email, password):
         self.first_name = first_name
         self.second_name = second_name
         self.email = email
         self.password = password
-        self.confirm_password = confirm_password
     def signup():
         first_name = input("Enter your first name: ")
         second_name = input("Enter your second name: ")
@@ -12,15 +11,22 @@ class Customer:
         password = input("Enter password: ")
         confirm_password = input("Confirm password: ")
         while password != confirm_password:
-            print("Passwords does not match: ")
+            print("Passwords do not match.")
             password = input("Enter password: ")
             confirm_password = input("Confirm password: ")
-        return Customer(first_name, second_name, email, password, confirm_password)
+        return Customer(first_name, second_name, email, password)
     def login():
         email = input("Enter your email: ")
         password = input("Enter password: ")
-        return Customer(email,password)
+        return Customer("", "", email, password)
 customer1 = Customer.signup()
 print(f"Sign up successful for {customer1.first_name}. Welcome to Groci store!")
 customer2 = Customer.login()
 print(f"Login successful for {customer2.email}. Welcome back to Groci store!")
+
+
+
+
+
+
+
